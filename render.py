c72609f5cd1d1b59146efbe56fbed9998493de71
Jeremy Muhlich <jmuhlich@bitflood.org>
2013-12-03 12:52:01 -0500

    refactored path/command templating in preparation for looping over all data

--- render.py
+++ render.py
@@ -19,20 +19,23 @@ CROP_2 = CROP_SIZE / 2
 OUTPUT_DIMS = (480,) * 2
 FRAME_DELTA = 5  # minutes
 
-input_frame_path = 'frames/r00_c00'
-processed_frame_path = input_frame_path + '/processed'
+input_frame_base = 'frames'
+processed_frame_subdir = 'processed'
 temp_movie_filename = 'output-temp.mp4'
 output_movie_filename = 'output.mp4'
 
 font = ImageFont.truetype('LiberationSans-Regular.ttf', 30)
 
-render_command = (
+render_command_template = (
     'ffmpeg -i %s/%%03d.jpg '
     '-y -vcodec libx264 -pix_fmt yuv420p -crf 25 -an '
-    '%s' % (processed_frame_path, temp_movie_filename)
+    '%s'
     )
-faststart_command ='qt-faststart %s %s' % (temp_movie_filename,
-                                           output_movie_filename)
+faststart_command_template ='qt-faststart %s %s'
+
+well_directory = 'r00_c00'
+input_frame_path = os.path.join(input_frame_base, well_directory)
+processed_frame_path = os.path.join(input_frame_path, processed_frame_subdir)
 makedirs_exist_ok(processed_frame_path)
 jpg_filenames = [p for p in os.listdir(input_frame_path) if p.endswith('.jpg')]
 timestamp_minutes = 0
@@ -51,6 +54,10 @@ for i, filename in enumerate(sorted(jpg_filenames)):
     image_out.save(output_filename, quality=95)
     timestamp_minutes += FRAME_DELTA
 print
-subprocess.check_call(render_command.split(' '))
-subprocess.check_call(faststart_command.split(' '))
+render_command = (render_command_template %
+                  (processed_frame_path, temp_movie_filename)).split(' ')
+faststart_command = (faststart_command_template %
+                     (temp_movie_filename, output_movie_filename)).split(' ')
+subprocess.check_call(render_command)
+subprocess.check_call(faststart_command)
 os.unlink(temp_movie_filename)
