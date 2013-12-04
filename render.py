f1b35509a178ea6e5adcadb6deb3c31a653c7847
Jeremy Muhlich <jmuhlich@bitflood.org>
2013-12-04 13:15:39 -0500

    use actual deltaT for timestamps

--- render.py
+++ render.py
@@ -3,7 +3,7 @@ import os
 import errno
 import subprocess
 import datetime
-
+import pickle
 from PIL import Image, ImageFont, ImageDraw
 
 
@@ -17,7 +17,6 @@ def makedirs_exist_ok(name):
 CROP_SIZE = 700
 CROP_2 = CROP_SIZE / 2
 OUTPUT_DIMS = (480,) * 2
-FRAME_DELTA = 5  # minutes
 
 input_frame_base = 'frames'
 processed_frame_subdir = 'processed'
@@ -38,21 +37,23 @@ input_frame_path = os.path.join(input_frame_base, well_directory)
 processed_frame_path = os.path.join(input_frame_path, processed_frame_subdir)
 makedirs_exist_ok(processed_frame_path)
 jpg_filenames = [p for p in os.listdir(input_frame_path) if p.endswith('.jpg')]
-timestamp_minutes = 0
+dt_filename = os.path.join(input_frame_path, 'delta_t.pck')
+delta_t = pickle.load(open(dt_filename))
 num_files = len(jpg_filenames)
-for i, filename in enumerate(sorted(jpg_filenames)):
-    print "\r%s  %d/%d" % (filename, i+1, num_files),
+for frame, filename in enumerate(sorted(jpg_filenames)):
+    print "\r%s  %d/%d" % (filename, frame+1, num_files),
     sys.stdout.flush()
     image_in = Image.open(os.path.join(input_frame_path, filename))
     (w, h) = image_in.size
     crop_box = (w/2-CROP_2, h/2-CROP_2, w/2+CROP_2, h/2+CROP_2)
     image_out = image_in.crop(crop_box).resize(OUTPUT_DIMS, Image.BILINEAR)
     draw = ImageDraw.Draw(image_out)
-    timestamp_text = '%02d:%02d' % divmod(timestamp_minutes, 60)
+    dt_minutes = delta_t[frame] / 60
+    ts_hours, ts_minutes = divmod(dt_minutes, 60)
+    timestamp_text = '%02d:%02d (%04d)' % (ts_hours, ts_minutes, dt_minutes)
     draw.text((10, 10), timestamp_text, font=font)
     output_filename = os.path.join(processed_frame_path, filename)
     image_out.save(output_filename, quality=95)
-    timestamp_minutes += FRAME_DELTA
 print
 render_command = (render_command_template %
                   (processed_frame_path, temp_movie_filename)).split(' ')
